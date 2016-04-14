9e71b58c333228bf4f857e78cb7ccb73791700f2
Riken Patel <Riken Patel>
2016-04-14 00:26:10 -0700

    first

---
+++ components/toolbar/toolbar.dir.js
@@ -0,0 +1,21 @@
+(function() {
+    
+    'use strict';
+    
+    angular
+    .module('authApp')
+    .directive ('toolbar',toolbar);
+
+    function toolbar() {
+        return{
+            templateUrl: 'components/toolbar/toolbar.tpl.html',
+            controller: toolbarController,
+            controllerAs: 'toolbar'
+        }
+    }
+   
+    
+    function toolbarController() {
+        
+    }
+}) ();
